763ea10471c0119f01716149360e8a6ad8fac9e0
Yuriy Borys <iam618384@gmail.com>
2019-12-15 23:31:38 +0200

    Update commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const perPage = 20;
 
 export default class App extends Component {
     state = {
+        isLoading: false,
         movies: []
     }
 
@@ -17,36 +18,45 @@ export default class App extends Component {
         let { movies } = this.state;
         let page = movies.length / perPage + 1;
         let url = `https://api.themoviedb.org/3/discover/movie?api_key=cd53ee965e63158d218cc6de9f207988&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`;
+
+        this.setState({
+            isLoading: true
+        })
+
         let response = await fetch(url);
         let json = await response.json();
         let data = json.results;
 
         this.setState({
+            isLoading: false,
             movies: [...movies, ...data]
         })
     }
 
     render() {
-        let { movies } = this.state;
+        let { isLoading, movies } = this.state;
 
         return (
             <div>
-                {movies.map(movie =>
+                {isLoading
+                ? <div>Loading...</div>
+                : movies.map(movie =>
                     <div id={movie.id} className={classes.clear}>
                         <img
                             alt=""
                             className={classes.tile}
                             src={"http://image.tmdb.org/t/p/original/" + movie.backdrop_path}/>
-                        <div className={classes.info}>
-                            <h3>{movie.title}</h3>
-                            <p>Score: {movie.vote_average} / 10</p>
-                            <details>
-                                <summary>Overview</summary>
-                                {movie.overview}
-                            </details>
+                            <div className={classes.info}>
+                                <h3>{movie.title}</h3>
+                                <p>Score: {movie.vote_average} / 10</p>
+                                <details>
+                                    <summary>Overview</summary>
+                                    {movie.overview}
+                                </details>
+                            </div>
                         </div>
-                    </div>
-                )}
+                    )
+                }
                 <button type="button" onClick={this.loadMore}>
                     Next {perPage}
                 </button>
