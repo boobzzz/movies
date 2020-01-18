aacba47f72436f4a80fd34e13abc1d67df845648
Yuriy Borys <iam618384@gmail.com>
2020-01-18 16:20:58 +0200

    Improve source structure

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
-import toPairs from "@bit/ramda.ramda.to-pairs";
+import values from "@bit/ramda.ramda.values";
 import MoviesList from "./components/MoviesList/MoviesList";
 import "./App.module.css";
 
 const urls = {
-    "Trending Now": "https://api.themoviedb.org/3/discover/movie",
-    "Sci-Fi Hits": "https://api.themoviedb.org/3/genre/878/movies",
-    "Comedy Hits": "https://api.themoviedb.org/3/genre/35/movies",
+    trendingNow: ["Trending Now", "https://api.themoviedb.org/3/discover/movie"],
+    scifiHits: ["Sci-Fi Hits", "https://api.themoviedb.org/3/genre/878/movies"],
+    comedyHits: ["Comedy Hits", "https://api.themoviedb.org/3/genre/35/movies"],
 }
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                {toPairs(urls).map(item =>
+                {values(urls).map(item =>
                     <section>
                         <h2>{item[0]}</h2>
                         <MoviesList url={item[1]} />
