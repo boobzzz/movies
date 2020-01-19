85ccb10113f2d18a10b52bbe96c30e45f4d6b3d9
Yuriy Borys <iam618384@gmail.com>
2020-01-19 19:22:47 +0200

    Add lib func fetchJSON

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-
-import values from "@bit/ramda.ramda.values";
 import toPairs from '@bit/ramda.ramda.to-pairs';
-
 import MoviesList from './components/MoviesList/MoviesList';
-import Button from './components/Button/Button';
 import './App.module.css';
 
 const urls = {
@@ -12,53 +8,15 @@ const urls = {
     'Sci-Fi Hits': 'https://api.themoviedb.org/3/genre/878/movies',
     'Comedy Hits': 'https://api.themoviedb.org/3/genre/35/movies',
 }
-const api_key = 'cd53ee965e63158d218cc6de9f207988'
-const perPage = 20;
 
 export default class App extends Component {
-    state = {
-        isLoading: false,
-        movies: []
-    }
-
-    // componentDidMount = async () => {
-    //     await values(urls).map(url => this.loadMore(url))
-    // }
-
-    loadMore = async (url) => {
-        let { movies } = this.state;
-        let page = Math.floor(movies.length / perPage) + 1;
-        let path = url + `?api_key=${api_key}&page=${page}`;
-        this.setState({
-            isLoading: true
-        })
-
-        let response = await fetch(path);
-        let json = await response.json();
-        let data = json.results;
-        console.log(data);
-
-        this.setState({
-            isLoading: false,
-            movies: [...movies, ...data]
-        })
-    }
-
     render() {
-        let { isLoading, movies } = this.state;
-
         return (
             <div>
-                {/* isLoading
-                ? <div>Loading...</div> */}
                 {toPairs(urls).map(item =>
                     <section>
                         <h2>{item[0]}</h2>
-                        <MoviesList movies={movies} />
-                        <Button
-                            type="button"
-                            itemsPerPage={perPage}
-                            clicked={() => this.loadMore(item[1])} />
+                        <MoviesList url={item[1]} />
                     </section>
                 )}
             </div>
--- src/components/MoviesList/MoviesList.js
+++ src/components/MoviesList/MoviesList.js
@@ -1,26 +1,70 @@
-import React from 'react';
-
+import React , { Component } from 'react';
+import fetchJSON from '../../lib/api.js';
+import Button from '../Button/Button';
 import classes from './MoviesList.module.css';
 
-const MoviesList = (props) => {
-    return (
-        props.movies.map(movie =>
-            <div id={movie.id} className={classes.clear}>
-                <img
-                    alt=""
-                    className={classes.tile}
-                    src={"http://image.tmdb.org/t/p/original/" + movie.backdrop_path}/>
-                <div className={classes.info}>
-                    <h3>{movie.title}</h3>
-                    <p>Score: {movie.vote_average} / 10</p>
-                    <details>
-                        <summary>Overview</summary>
-                        {movie.overview}
-                    </details>
-                </div>
+const options = {
+    headers: {
+        "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjZDUzZWU5NjVlNjMxNThkMjE4Y2M2ZGU5ZjIwNzk4OCIsInN1YiI6IjVkZjY2MjBmMGQxZTdmMDAxNTcxZjEyNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EmR74K8s26d5IUQwwyDtxY8DZS6WLoLbeB8rvZH6mGc",
+    }
+}
+const perPage = 20;
+
+export default class MoviesList extends Component {
+    state = {
+        isLoading: false,
+        movies: []
+    }
+
+    componentDidMount = async () => {
+        await this.loadMore(this.props.url)
+    }
+
+    loadMore = async () => {
+        let { movies } = this.state;
+        let page = Math.floor(movies.length / perPage) + 1;
+        let path = this.props.url + `?page=${page}`;
+
+        this.setState({
+            isLoading: true
+        })
+
+        let data = await fetchJSON(path, options)
+
+        this.setState({
+            isLoading: false,
+            movies: [...movies, ...data.body.results]
+        })
+    }
+
+    render() {
+        let { isLoading, movies } = this.state;
+
+        return (
+            isLoading
+            ? <div>Loading...</div>
+            : <div>
+                {movies.map(movie =>
+                    <div id={movie.id} className={classes.clear}>
+                        <img
+                            alt=""
+                            className={classes.tile}
+                            src={"http://image.tmdb.org/t/p/original/" + movie.backdrop_path}/>
+                        <div className={classes.info}>
+                            <h3>{movie.title}</h3>
+                            <p>Score: {movie.vote_average} / 10</p>
+                            <details>
+                                <summary>Overview</summary>
+                                {movie.overview}
+                            </details>
+                        </div>
+                    </div>
+                )}
+                <Button
+                    itemsPerPage={this.props.itemsPerPage}
+                    clicked={this.loadMore} />
+                <hr/>
             </div>
         )
-    )
+    }
 }
-
-export default MoviesList;
