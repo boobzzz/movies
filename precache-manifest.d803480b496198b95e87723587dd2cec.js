16cacbf93223f091cfccd2b70578c7fc7f07b45c
Yuriy Borys <iam618384@gmail.com>
2020-07-17 00:29:01 +0300

    Updates

---
+++ precache-manifest.d803480b496198b95e87723587dd2cec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "895a28bb935539efea973ab9d54f7a08",
+    "url": "/movies/index.html"
+  },
+  {
+    "revision": "9c0134f72a2f58e7df9e",
+    "url": "/movies/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "14aec9c3577ec65dd492",
+    "url": "/movies/static/css/main.cf8b3256.chunk.css"
+  },
+  {
+    "revision": "9c0134f72a2f58e7df9e",
+    "url": "/movies/static/js/2.e8f69b16.chunk.js"
+  },
+  {
+    "revision": "c6a378f9a058859e741f4b4635a913f0",
+    "url": "/movies/static/js/2.e8f69b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14aec9c3577ec65dd492",
+    "url": "/movies/static/js/main.26df9302.chunk.js"
+  },
+  {
+    "revision": "49c503936debd188d675",
+    "url": "/movies/static/js/runtime-main.fb726eca.js"
+  },
+  {
+    "revision": "08392f186e99a74794065effff6f90e5",
+    "url": "/movies/static/media/footer-logo.08392f18.svg"
+  },
+  {
+    "revision": "31d26327470d3d7f96f7bed3417fe659",
+    "url": "/movies/static/media/header-logo.31d26327.svg"
+  },
+  {
+    "revision": "f34a936c8b099cd8f9c70852f488be50",
+    "url": "/movies/static/media/image_not_found.f34a936c.png"
+  }
+]);
